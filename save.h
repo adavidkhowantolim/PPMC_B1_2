c713643d3dd8579c2ba3a8f735d91af69ae8a0d4
princess197 <49345865+princess197@users.noreply.github.com>
2019-04-09 09:43:03 +0700

    Update save.h

--- save.h
+++ save.h
@@ -3,6 +3,15 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct {
+	char kode_praktikum[10];
+	char Rombongan[10];
+	int count_asisten;
+	char nama_asisten[3];
+} jadwal_t;
+
+jadwal_t LAB1[12][5], LAB2[12][5], LAB3[12][5], LSS[12][5];
+
 char* konversiString(int b);
 void Simpan (char *filename);
 
